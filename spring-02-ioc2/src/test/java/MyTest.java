d90d53db85d47afcda930a409e2ab52f2205b0cb
Kyloren96 <Dyhfr96@outlook.com>
2021-03-07 01:12:55 +0800

    spring ioc constructor with args

--- spring-02-ioc2/src/test/java/MyTest.java
+++ spring-02-ioc2/src/test/java/MyTest.java
@@ -1,4 +1,5 @@
 import com.dyh.pojo.User;
+import com.dyh.pojo.UserT;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -7,6 +8,8 @@ public class MyTest {
     public void test1() {
         ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("beans.xml");
         User user = (User) classPathXmlApplicationContext.getBean("user");
+        User userT = (User) classPathXmlApplicationContext.getBean("user");
+        System.out.println(user == userT);
         user.show();
     }
 }
