e20fc9fc604e065a679034f8ad07ffe61aa38e56
Kyloren96 <Dyhfr96@outlook.com>
2021-03-13 23:28:07 +0800

    mybatis config

---
+++ spring-10-mybatis/src/test/java/MyTest.java
@@ -0,0 +1,28 @@
+import com.dyh.mapper.UserMapper;
+import com.dyh.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class MyTest {
+    @Test
+    public void test() throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+
+
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        List<User> users = mapper.selectUser();
+        for (User user : users) {
+            System.out.println(user.toString());
+        }
+    }
+}
